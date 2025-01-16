d2b3154f4cf8d9f7e17a3ad7ba14ea94f94a2e11
TimoumiWejden <ttwejden@gmail.com>
2025-01-16 19:26:48 +0100

    "commit"

--- Devop-Front/src/app/app.component.spec.ts
+++ Devop-Front/src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+
+//describe
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
